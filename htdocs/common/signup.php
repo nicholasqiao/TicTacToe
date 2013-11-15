cf1f4ec866d2fc10e0a85b6cd7899b01439d526d
Eric Stanton <estantondev@gmail.com>
2013-11-14 20:59:07 -0500

    Made some changes to linking so that php files are found in the common folder. Also changed the config file for my own path, please change when you pull to change root.

--- htdocs/common/signup.php
+++ htdocs/common/signup.php
@@ -8,11 +8,7 @@ $password = $_REQUEST['password'];
 
 $uid = User::newUser($username,$password);
 
-if (uid < 0)
-	echo '<!DOCTYPE HTML><html><head><title>Username Taken</title></head><body>That username is taken. <a href="/signup.html">Go Back</a></body></html>';
-else {
-	session_start();
-	$_SESSION['uid'] = $uid;
-	
-	header('Location: /common/test/afterlogin');
-}
+if ($uid < 0)
+	echo '<!DOCTYPE HTML><html><head><title>Username Taken</title></head><body>That username is taken. <a href="../signup.html">Go Back</a></body></html>';
+else 
+	echo '<!DOCTYPE HTML><html><head><title>Username Taken</title></head><body>Account successfully created!. <a href="../home.html">Go Back</a></body></html>';
